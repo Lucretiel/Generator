b96fefffc9f6979dd5a7160510e00867b64d71bb
Nathan West <Lucretiel@gmail.com>
2013-02-13 02:27:01 -0500

    Added GeneratorIteratorConst

--- GeneratorIterator.h
+++ GeneratorIterator.h
@@ -11,6 +11,11 @@ using GeneratorIterator =
 		Generator<T>,
 		typename Generator<T>::value_type>;
 		
+template<class T>
+using GeneratorIteratorConst =
+	GeneratorIteratorGeneric<
+		Generator<T>,
+		typename Generator<T>::value_type const>;
 
 //in C++11, these can be used in place of .begin() and .end()
 template<class T>
@@ -24,5 +29,21 @@ GeneratorIterator<T> end(Generator<T>& gen)
 {
 	return GeneratorIterator<T>();
 }
+
+template<class T>
+GeneratorIteratorConst<T> cbegin(Generator<T>& gen)
+{
+	return GeneratorIteratorConst<T>(&gen);
+}
+
+template<class T>
+GeneratorIteratorConst<T> cend(Generator<T>& gen)
+{
+	return GeneratorIteratorConst<T>();
+}
+/*
+ * Would rather have used a template here, but the savings for these one-
+ * liners are negligible
+ */
 
 #endif /* MAKE_ITERATOR_H_ */
