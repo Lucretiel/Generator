6922aba2cdc84f2bee205b342eb27346012d7d52
Nathan West <Lucretiel@gmail.com>
2013-02-13 14:09:21 -0500

    Added eclipse-generated makefiles

---
+++ Debug/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../main.cpp 
+
+OBJS += \
+./main.o 
+
+CPP_DEPS += \
+./main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -D__GXX_EXPERIMENTAL_CXX0X__ -D__cplusplus=201103L -I/usr/local/include/ -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
