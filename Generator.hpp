5c2e5dc57a59bf07d98980486027fc9821ec185f
Nathan West <nathan@mimedia.com>
2013-02-14 13:04:01 -0500

    Created new template-based yield implementation

--- Generator.hpp
+++ Generator.hpp
@@ -24,7 +24,7 @@
 //The actual stack size will be this or minimum_stacksize, whichever is larger
 const unsigned default_stack_size = 256;
 
-template<class YieldType>
+template<class YieldType, class Allocator = boost::context::guarded_stack_allocator>
 class Generator : public GeneratorInterface<YieldType>
 {
 public:
@@ -32,16 +32,21 @@ public:
 	typedef yield_type* yield_ptr_type;
 
 private:
-	//Return states from a next into a yield
-	enum class YieldBack : intptr_t {Resume, Stop};
-
-	//thrown from yield() when the context must be immediately destroyed.
-	class ImmediateStop {}; //For throwing out of yield()
-
+	//Internal storage and state types
 	//Storage for either a yield_type or pointer to a yield_type
 	typedef boost::variant<
 			yield_ptr_type,
 			yield_type> YieldTypeStorage;
+
+	typedef ManagedStack<Allocator> Stack;
+	typedef boost::context::fcontext_t context_type;
+
+private:
+	//Return states from a next into a yield
+	enum class YieldBack : intptr_t {Resume, Stop};
+
+	//thrown from yield() when the context must be immediately destroyed.
+	class ImmediateStop {};
 
 	struct yield_value_getter : public boost::static_visitor<yield_ptr_type>
 	{
@@ -56,46 +61,60 @@ private:
 	};
 
 private:
-	ManagedStack<boost::context::guarded_stack_allocator> inner_stack;
-	boost::context::fcontext_t* inner_context;
-	boost::context::fcontext_t outer_context;
+	Stack inner_stack;
+	context_type* inner_context;
+	context_type outer_context;
 	YieldTypeStorage yield_value;
 	bool started;
+
+private:
+	//Wrappers for launching the coroutine
 
 	//initiator function to fit into make_fcontext
 	static void static_run(intptr_t p)
 	{
-		reinterpret_cast<Generator*>(p)->begin_run();
+		context* outer = reinterpret_cast<Generator*>(p)->begin_run();
+		context* inner;
+
+		boost::jump_fcontext(inner, outer, 0);
+		//TODO: determine what happens if this simply returns
 	}
 
 	/*
-	 * Wrapper for the virtual run. Handles exceptions and outermost context
-	 * switching
+	 * Primary wrapper. Launches coroutine, handles ImmediateStop exceptions,
+	 * and cleans up on an exit
 	 */
-	void begin_run()
+	context_type* begin_run()
 	{
 		started = true;
 
 		try { run(); }
 		catch(ImmediateStop&) {}
+
 		yield_value = nullptr;
 
-		//While true here in case this context is resumed after returning
-		while(true)
-		{
-			boost::context::jump_fcontext(inner_context, &outer_context, 0);
-		}
+		//Point of no return
+		inner_context = nullptr;
+
+		return &outer_context;
 	}
 
 	//The actual run implementation. This is the one you care about, clients!
 	virtual void run() =0;
+	//TODO: consider using a function pointer or CRTP instead of a virtual
+
+protected:
+	//Yields
 
 	/*
-	 * Execute a context switch OUT of the generator. Yield object assignment
-	 * is handled elsewhere
+	 * Primary yield function. T should be convertible to yield_type or
+	 * yield_ptr_type. Move aware.
 	 */
-	void yield_internal()
+	template<class T>
+	void yield(T&& obj)
 	{
+		yield_value = std::forward<T>(obj);
+
 		auto result = static_cast<YieldBack>(
 				boost::context::jump_fcontext(
 						inner_context, &outer_context, 0));
@@ -103,72 +122,6 @@ private:
 		if(result == YieldBack::Stop)
 			throw ImmediateStop();
 	}
-
-	//Executes a context switch into the generator
-	void yield_back_jump(intptr_t x)
-	{
-		boost::context::jump_fcontext(&outer_context, inner_context, x);
-	}
-
-	/*
-	 * Handler for switching into the generator. Handles sending in yield_back,
-	 * casts, etc.
-	 */
-	void yield_back_internal(YieldBack yield_back = YieldBack::Resume)
-	{
-		if(started)
-		{
-			yield_back_jump(static_cast<intptr_t>(yield_back));
-		}
-		else
-		{
-			if(yield_back == YieldBack::Resume)
-				yield_back_jump(reinterpret_cast<intptr_t>(this));
-		}
-	}
-
-	//End the coroutine. This is the safe one.
-	void cleanup()
-	{
-		if(inner_context)
-		{
-			yield_back_internal(YieldBack::Stop);
-			clear_internal_context();
-		}
-	}
-
-	//Destroy the coroutine. This is the unsafe on. Use cleanup() instead.
-	void clear_internal_context()
-	{
-		inner_context = nullptr;
-		inner_stack.clear();
-		yield_value = nullptr;
-	}
-
-protected:
-	//TODO: investigate template implementation of yield
-	//Yield a value by reference. Copies the value to internal storage.
-	void yield(const yield_type& obj)
-	{
-		yield_value = obj;
-		yield_internal();
-	}
-
-	//Yield by rvalue reference. Moves the value to internal storage.
-	void yield(yield_type&& obj)
-	{
-		yield_value = std::move(obj);
-		yield_internal();
-	}
-
-	//TODO: investigate corner case with yielding nullptr
-	//Yield by pointer. Copies the pointer into internal storage
-	void yield(yield_type* ptr)
-	{
-		yield_value = ptr;
-		yield_internal();
-	}
-	//NOTE: yielding nullptr will end the coroutine.
 
 	//Yield from a generator. Only returns when gen is finished.
 	void yield_from(GeneratorInterface<yield_type>& gen)
@@ -182,6 +135,61 @@ protected:
 	void yield_from(GeneratorInterface<yield_type>&& gen)
 	{
 		yield_from(gen);
+	}
+
+	//yield from some other iterable
+	template<class T>
+	void yield_from(T&& obj)
+	{
+		for(auto& item : obj)
+		{
+			yield(&item);
+		}
+	}
+
+private:
+	//Functions relating to managing the coroutine (switching into it, etc)
+
+	//Executes a context switch into the generator
+	void yield_back_jump(intptr_t x)
+	{
+		boost::context::jump_fcontext(&outer_context, inner_context, x);
+	}
+
+	/*
+	 * Handler for switching into the generator. Handles sending in yield_back,
+	 * casts, etc. Returns pointer to the yielded object.
+	 */
+	yield_ptr_type yield_back_internal(YieldBack yield_back = YieldBack::Resume)
+	{
+		if(started)
+		{
+			yield_back_jump(static_cast<intptr_t>(yield_back));
+		}
+		else
+		{
+			if(yield_back == YieldBack::Resume)
+				yield_back_jump(reinterpret_cast<intptr_t>(this));
+		}
+		return boost::apply_visitor(yield_value_getter(), yield_value);
+	}
+
+	//End the coroutine. This is the safe one.
+	void cleanup()
+	{
+		if(inner_context)
+		{
+			yield_back_internal(YieldBack::Stop);
+			clear_internal_context();
+		}
+	}
+
+	//Destroy the coroutine. This is the unsafe one. Use cleanup() instead.
+	void clear_internal_context()
+	{
+		inner_context = nullptr;
+		inner_stack.clear();
+		yield_value = nullptr;
 	}
 
 public:
@@ -204,9 +212,10 @@ public:
 	Generator& operator=(const Generator&) =delete;
 
 	/*
-	 * Note that move constructing or assigning a generator will invalidate any
+	 * Note that move constructing or assigning a generator MAY invalidate any
 	 * return value pointers from next(), because the pointed-to value has
-	 * moved to the new generator.
+	 * moved to the new generator. If a pointer was yielded from the coroutine,
+	 * rather than a full object, it should be safe
 	 */
 	Generator(Generator&& mve):
 		inner_stack(std::move(mve.inner_stack)),
@@ -244,9 +253,8 @@ public:
 			return nullptr;
 
 		yield_value = nullptr;
-		yield_back_internal();
+		yield_ptr_type ret(yield_back_internal());
 
-		auto ret = boost::apply_visitor(yield_value_getter(), yield_value);
 		if(!ret) cleanup();
 		return ret;
 	}
